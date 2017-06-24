1119c8611a56623ef4eb28b65086160ed9adfe8f
saguilare <stevenaguilar@Stevens-MacBook-Pro.local>
2017-06-24 14:40:37 -0600

    update ctlr and bll

--- SIGELIBMA/Controllers/MantRolesController.cs
+++ SIGELIBMA/Controllers/MantRolesController.cs
@@ -10,21 +10,21 @@ namespace SIGELIBMA.Controllers
 {
     public class MantRolesController : Controller
     {
-        private RoleService roleService;
+        private RolService roleService = new RolService();
 
-        // GET: MantRoles
+
+        [HttpGet]
         public ActionResult Index()
-        {
-      
+        { 
             return View();
         }
 
         [HttpGet]
-        public JsonResult  GetRoles() {
+        public JsonResult GetAll() {
+
             try
             {
-                RoleService service = new RoleService();
-                List<Role> roles = service.GetAll();
+                List<Role> roles = roleService.GetAll();
                 return Json(new { OperationStatus = true, Roles = roles, Message = "Operation OK" },JsonRequestBehavior.AllowGet);
             }
             catch (Exception e)
@@ -34,5 +34,56 @@ namespace SIGELIBMA.Controllers
                 return Json(new { OperationStatus = false, Message = "Exception thrown, please verify backend services" }, JsonRequestBehavior.AllowGet);
             }
         }
+
+        [HttpPost]
+        public JsonResult GetById(Role rolp)
+        {
+            try
+            {
+                Role rol = roleService.GetById(rolp);
+                return Json(new { OperationStatus = true, Rol = rol, Message = "Operation OK" });
+            }
+            catch (Exception e)
+            {
+
+                //TODO handle ex
+                return Json(new { OperationStatus = false, Message = "Exception thrown, please verify backend services" });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Delete(Role rolp)
+        {
+            try
+            {
+                bool result = false;
+                result = roleService.Delete(rolp);
+                return Json(new { OperationStatus = true, Result = result, Message = "Operation OK" });
+            }
+            catch (Exception e)
+            {
+
+                //TODO handle ex
+                return Json(new { OperationStatus = false, Message = "Exception thrown, please verify backend services" });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Update(Role rolp)
+        {
+            try
+            {
+                bool result = false;
+                result = roleService.Update(rolp);
+                return Json(new { OperationStatus = true, Result = result, Message = "Operation OK" });
+            }
+            catch (Exception e)
+            {
+
+                //TODO handle ex
+                return Json(new { OperationStatus = false, Message = "Exception thrown, please verify backend services" });
+            }
+        }
+
     }
 }
